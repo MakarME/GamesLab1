c6981712b40fe47eb27e22e35973cd970baec288
Acer <b@gmail.com>
2024-03-04 14:53:50 +0200

    Before changing controllers

--- src/GamesMVC/GamesDomain/Model/Genre.cs
+++ src/GamesMVC/GamesDomain/Model/Genre.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GamesDomain.Model;
 
 public partial class Genre : Entity
 {
+    [Required(ErrorMessage = "THis field must not be empty")]
+    [Display(Name="Genre")]
     public string Name { get; set; } = null!;
 
+    [Display(Name = "Genre info")]
     public string? Info { get; set; }
 
     public virtual ICollection<Game> Games { get; set; } = new List<Game>();
